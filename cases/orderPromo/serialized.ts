81ab9833906de8785a738501669dab4c3e34d025
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2025-07-24 18:21:49 +0400

    chore: Удален неиспользуемый код

--- cases/orderPromo/serialized.ts
+++
@@ -1,7 +0,0 @@
-import type { SerializedEventType } from '@omnicajs/vue-remote/types/events'
-
-export const onSerializedEvent = <T extends Event>(handler: (event: SerializedEventType<T>) => void) => {
-    return (event: Event) => {
-        handler(event as SerializedEventType<T>)
-    }
-}
